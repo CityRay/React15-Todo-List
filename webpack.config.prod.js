0e5c735389715ed9a339989f803f8d6bc77d8d01
RayLin <ray102467@gmail.com>
2016-12-03 00:15:56 +0800

    Update webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,6 +37,7 @@ module.exports = {
     //     'react': 'React'
     // },
     plugins: [
+        new webpack.optimize.DedupePlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production'),
